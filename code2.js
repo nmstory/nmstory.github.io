daa726ae586e04237ae12934f727751698b5ab68
nmstory <n.m.story@newcastle.ac.uk>
2021-03-01 15:39:05 +0000

    Update

--- code2.js
+++ code2.js
@@ -1,21 +1,21 @@
-gdjs.EndgameLostCode = {};
-gdjs.EndgameLostCode.GDScreenObjects1= [];
-gdjs.EndgameLostCode.GDScreenObjects2= [];
+gdjs.EndgameWonCode = {};
+gdjs.EndgameWonCode.GDScreenObjects1= [];
+gdjs.EndgameWonCode.GDScreenObjects2= [];
 
-gdjs.EndgameLostCode.conditionTrue_0 = {val:false};
-gdjs.EndgameLostCode.condition0IsTrue_0 = {val:false};
-gdjs.EndgameLostCode.condition1IsTrue_0 = {val:false};
+gdjs.EndgameWonCode.conditionTrue_0 = {val:false};
+gdjs.EndgameWonCode.condition0IsTrue_0 = {val:false};
+gdjs.EndgameWonCode.condition1IsTrue_0 = {val:false};
 
 
-gdjs.EndgameLostCode.eventsList0 = function(runtimeScene) {
+gdjs.EndgameWonCode.eventsList0 = function(runtimeScene) {
 
 {
 
 
-gdjs.EndgameLostCode.condition0IsTrue_0.val = false;
+gdjs.EndgameWonCode.condition0IsTrue_0.val = false;
 {
-gdjs.EndgameLostCode.condition0IsTrue_0.val = gdjs.evtTools.input.isKeyPressed(runtimeScene, "Space");
-}if (gdjs.EndgameLostCode.condition0IsTrue_0.val) {
+gdjs.EndgameWonCode.condition0IsTrue_0.val = gdjs.evtTools.input.isKeyPressed(runtimeScene, "Space");
+}if (gdjs.EndgameWonCode.condition0IsTrue_0.val) {
 {gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "Splash Screen", false);
 }}
 
@@ -24,15 +24,15 @@ gdjs.EndgameLostCode.condition0IsTrue_0.val = gdjs.evtTools.input.isKeyPressed(r
 
 };
 
-gdjs.EndgameLostCode.func = function(runtimeScene) {
+gdjs.EndgameWonCode.func = function(runtimeScene) {
 runtimeScene.getOnceTriggers().startNewFrame();
 
-gdjs.EndgameLostCode.GDScreenObjects1.length = 0;
-gdjs.EndgameLostCode.GDScreenObjects2.length = 0;
+gdjs.EndgameWonCode.GDScreenObjects1.length = 0;
+gdjs.EndgameWonCode.GDScreenObjects2.length = 0;
 
-gdjs.EndgameLostCode.eventsList0(runtimeScene);
+gdjs.EndgameWonCode.eventsList0(runtimeScene);
 return;
 
 }
 
-gdjs['EndgameLostCode'] = gdjs.EndgameLostCode;
+gdjs['EndgameWonCode'] = gdjs.EndgameWonCode;
--- code3.js
+++ code3.js
@@ -1,21 +1,21 @@
-gdjs.EndgameWonCode = {};
-gdjs.EndgameWonCode.GDScreenObjects1= [];
-gdjs.EndgameWonCode.GDScreenObjects2= [];
+gdjs.EndgameLostCode = {};
+gdjs.EndgameLostCode.GDScreenObjects1= [];
+gdjs.EndgameLostCode.GDScreenObjects2= [];
 
-gdjs.EndgameWonCode.conditionTrue_0 = {val:false};
-gdjs.EndgameWonCode.condition0IsTrue_0 = {val:false};
-gdjs.EndgameWonCode.condition1IsTrue_0 = {val:false};
+gdjs.EndgameLostCode.conditionTrue_0 = {val:false};
+gdjs.EndgameLostCode.condition0IsTrue_0 = {val:false};
+gdjs.EndgameLostCode.condition1IsTrue_0 = {val:false};
 
 
-gdjs.EndgameWonCode.eventsList0 = function(runtimeScene) {
+gdjs.EndgameLostCode.eventsList0 = function(runtimeScene) {
 
 {
 
 
-gdjs.EndgameWonCode.condition0IsTrue_0.val = false;
+gdjs.EndgameLostCode.condition0IsTrue_0.val = false;
 {
-gdjs.EndgameWonCode.condition0IsTrue_0.val = gdjs.evtTools.input.isKeyPressed(runtimeScene, "Space");
-}if (gdjs.EndgameWonCode.condition0IsTrue_0.val) {
+gdjs.EndgameLostCode.condition0IsTrue_0.val = gdjs.evtTools.input.isKeyPressed(runtimeScene, "Space");
+}if (gdjs.EndgameLostCode.condition0IsTrue_0.val) {
 {gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "Splash Screen", false);
 }}
 
@@ -24,15 +24,15 @@ gdjs.EndgameWonCode.condition0IsTrue_0.val = gdjs.evtTools.input.isKeyPressed(ru
 
 };
 
-gdjs.EndgameWonCode.func = function(runtimeScene) {
+gdjs.EndgameLostCode.func = function(runtimeScene) {
 runtimeScene.getOnceTriggers().startNewFrame();
 
-gdjs.EndgameWonCode.GDScreenObjects1.length = 0;
-gdjs.EndgameWonCode.GDScreenObjects2.length = 0;
+gdjs.EndgameLostCode.GDScreenObjects1.length = 0;
+gdjs.EndgameLostCode.GDScreenObjects2.length = 0;
 
-gdjs.EndgameWonCode.eventsList0(runtimeScene);
+gdjs.EndgameLostCode.eventsList0(runtimeScene);
 return;
 
 }
 
-gdjs['EndgameWonCode'] = gdjs.EndgameWonCode;
+gdjs['EndgameLostCode'] = gdjs.EndgameLostCode;
